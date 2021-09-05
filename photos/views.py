d1f6f3f2c152cbe59d9409bcaf9724b2b0e316a4
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-09-05 21:18:40 +0300

    views edit

--- photos/views.py
+++ photos/views.py
@@ -1,97 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Category, Photo
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
-# Create your views here.
+from .models import Image, Location
 
 
-def loginUser(request):
-    page = 'login'
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('gallery')
-
-    return render(request, 'photos/login_register.html', {'page': page})
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
 
 
-def logoutUser(request):
-    logout(request)
-    return redirect('login')
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pictures/location.html', {'location_images': images})
 
 
-def registerUser(request):
-    page = 'register'
-    form = CustomUserCreationForm()
-
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-
-            if user is not None:
-                login(request, user)
-                return redirect('gallery')
-
-    context = {'form': form, 'page': page}
-    return render(request, 'photos/login_register.html', context)
-
-
-@login_required(login_url='login')
-def gallery(request):
-    user = request.user
-    category = request.GET.get('category')
-    if category == None:
-        photos = Photo.objects.filter(category__user=user)
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
     else:
-        photos = Photo.objects.filter(
-            category__name=category, category__user=user)
-
-    categories = Category.objects.filter(user=user)
-    context = {'categories': categories, 'photos': photos}
-    return render(request, 'photos/gallery.html', context)
-
-
-@login_required(login_url='login')
-def viewPhoto(request, pk):
-    photo = Photo.objects.get(id=pk)
-    return render(request, 'photos/photo.html', {'photo': photo})
-
-
-@login_required(login_url='login')
-def addPhoto(request):
-    user = request.user
-
-    categories = user.category_set.all()
-
-    if request.method == 'POST':
-        data = request.POST
-        images = request.FILES.getlist('images')
-
-        if data['category'] != 'none':
-            category = Category.objects.get(id=data['category'])
-        elif data['category_new'] != '':
-            category, created = Category.objects.get_or_create(
-                user=user,
-                name=data['category_new'])
-        else:
-            category = None
-
-        for image in images:
-            photo = Photo.objects.create(
-                category=category,
-                description=data['description'],
-                image=image,
-            )
-
-        return redirect('gallery')
-
-    context = {'categories': categories}
-    return render(request, 'photos/add.html', context)
+        message = "You haven't searched for any image category"
+        return render(request, 'pictures/search_results.html', {"message": message})
