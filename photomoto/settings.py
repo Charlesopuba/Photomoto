1354b148eeffb464a6840bdaf4391aff9f7a868d
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-09-05 20:31:13 +0300

    more changes

--- photomoto/settings.py
+++ photomoto/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'photomoto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa,Nairobi'
 
 USE_I18N = True
 
