b2b8234034025f6a6e99e28e2abf98de2428eafb
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-09-06 11:41:35 +0300

    prep deployment

--- photomoto/settings.py
+++ photomoto/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+MEDIA_URL = '/gallery/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'gallery')
 
 STATIC_URL = '/static/'
 
